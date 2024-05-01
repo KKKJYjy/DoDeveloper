a8b14d4e391c8b6c937baaa8d3387c806939b285
webyyj <duswn7081@naver.com>
2024-05-01 21:19:12 +0900

    feat : 스터디 모임글 리스트를 불러오기 위한  StudyBoardVO 객체 생성

---
+++ src/main/java/com/dodeveloper/study/vodto/StudyBoardVO.java
@@ -0,0 +1,29 @@
+package com.dodeveloper.study.vodto;
+
+import java.sql.Timestamp;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class StudyBoardVO {
+	private String stuWriter;
+	private String stuTitle;
+	private String stuContent;
+	private String stuLoc;
+	private double stuX;
+	private double stuY;
+	private String stuDate;
+	private String stuPers; //이거 디비에도 변경
+	private String contactLink;
+	private String status;
+	private Timestamp endDate;
+}
