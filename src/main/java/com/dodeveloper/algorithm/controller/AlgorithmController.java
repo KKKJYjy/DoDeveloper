9911fb010ce8db098ef608e056b08a2a9f5636ce
mjimji <jeonil2072@gmail.com>
2024-05-02 17:59:11 +0900

    feat : 알고리즘게시판 controller,service,dao,vodto생성 - algorithm/listAll.jsp에 db에 있는  algBoard 출력 테스트 성공

--- src/main/java/com/dodeveloper/algorithm/controller/AlgorithmController.java
+++ src/main/java/com/dodeveloper/algorithm/controller/AlgorithmController.java
@@ -1,20 +1,40 @@
 package com.dodeveloper.algorithm.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dodeveloper.algorithm.service.AlgService;
+import com.dodeveloper.algorithm.vodto.AlgBoardDTO;
 
 @Controller
 @RequestMapping("/algorithm")
 public class AlgorithmController {
 	
+	@Autowired
+	AlgService aService;
+	
 	private static final Logger logger = LoggerFactory.getLogger(AlgorithmController.class);
 	
 	@GetMapping(value = "/listAll")
-	public void listAllGet() {
+	public void listAllGet(Model model) throws Exception { 
 		logger.info("listAll View.");
+		System.out.println("!!!컨트롤러!!!");
+		
+		List<AlgBoardDTO> returnMap = null;
+		
+		// 멤버테이블 출력함
+		returnMap = aService.getListAllBoard();
+		
+		
+		model.addAttribute("algBoardList",returnMap);
 	}
 
 }
