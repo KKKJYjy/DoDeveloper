0d252068164637400ef5891055c060612af01735
webyyj <duswn7081@naver.com>
2024-05-02 17:19:14 +0900

    feat : StuStackVO 객체 생성 각 스터디별 공부할 언어를 불러오기 위해서 생성

---
+++ src/main/java/com/dodeveloper/study/vodto/StuStackVO.java
@@ -0,0 +1,22 @@
+package com.dodeveloper.study.vodto;
+
+import java.sql.Timestamp;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class StuStackVO {
+	private int stuStackNo;
+	private int stuBoardNo;
+	private int chooseStack;
+	private String stackName;
+}
