59dc25abfc435538b6deadada4417aa6a55c33b8
kangjy <himgaso@gmail.com>
2024-05-02 16:26:20 +0900

    feat : 수령된 메세지 출력 기능 구현
    아이디를 인자로 받은 후, 해당 아이디로 발송된 메세지들을
    리스트 형태로 반환해주는 기능을 MessageService 로 구현

--- src/main/java/com/dodeveloper/message/service/MessageService.java
+++ src/main/java/com/dodeveloper/message/service/MessageService.java
@@ -1,7 +1,11 @@
 package com.dodeveloper.message.service;
 
+import java.util.List;
+
+import com.dodeveloper.message.vodto.MessageBoxVO;
 import com.dodeveloper.message.vodto.MessageVO;
 
 public interface MessageService {
 	MessageVO getMessageByNo(int messageNo) throws Exception;
+	List<MessageVO> getReceivedMessages(String receiver, int startPoint, int amountToShow) throws Exception;
 }
--- src/main/java/com/dodeveloper/message/service/MessageServiceImpl.java
+++ src/main/java/com/dodeveloper/message/service/MessageServiceImpl.java
@@ -1,13 +1,40 @@
 package com.dodeveloper.message.service;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dodeveloper.message.dao.MessageBoxDAO;
+import com.dodeveloper.message.dao.MessageDAO;
+import com.dodeveloper.message.vodto.MessageBoxVO;
 import com.dodeveloper.message.vodto.MessageVO;
 
+@Service
 public class MessageServiceImpl implements MessageService{
 
+	@Autowired
+	private MessageDAO messageDAO;
+
+	@Autowired
+	private MessageBoxDAO messageBoxDAO;
+	
 	@Override
 	public MessageVO getMessageByNo(int messageNo) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		return messageDAO.selectMessageByNo(messageNo);
+	}
+
+	@Override
+	public List<MessageVO> getReceivedMessages(String receiver, int startPoint, int amountToShow) throws Exception {
+		List<MessageVO> receivedMessages = new LinkedList<MessageVO>();
+		List<MessageBoxVO> receivedMessageInfo = messageBoxDAO.selectByReceiver(receiver, startPoint, amountToShow);
+		
+		for(MessageBoxVO messageInfo : receivedMessageInfo) {
+			receivedMessages.add(messageDAO.selectMessageByNo(messageInfo.getMessageNo()));
+		}
+		
+		return receivedMessages;
 	}
 
 }
