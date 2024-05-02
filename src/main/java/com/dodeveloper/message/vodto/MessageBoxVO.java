c246abe615fa65a35e44da6ae625902276f38520
kangjy <himgaso@gmail.com>
2024-05-02 16:37:43 +0900

    feat : 메세지 수령함 전용 DTO VO 생성
    messageBox 테이블을 위한 DTO 와 VO를 생성

---
+++ src/main/java/com/dodeveloper/message/vodto/MessageBoxVO.java
@@ -0,0 +1,21 @@
+package com.dodeveloper.message.vodto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+@ToString
+public class MessageBoxVO {
+	private int messageBoxNo;
+	private int messageNo;
+	private String receiver;
+	private String isRead;
+	private String isDeleted;
+}
