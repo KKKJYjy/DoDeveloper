6d5e82397443217fa369aab0eea861e9991bd362
kangjy <himgaso@gmail.com>
2024-05-03 14:30:28 +0900

    feat : 메세지 insert 기능 추가
    메세지를 insert 하는 기능을 DAO 단에 추가

--- src/main/java/com/dodeveloper/message/dao/MessageDAO.java
+++ src/main/java/com/dodeveloper/message/dao/MessageDAO.java
@@ -1,9 +1,14 @@
 package com.dodeveloper.message.dao;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
+import com.dodeveloper.message.vodto.MessageDTO;
 import com.dodeveloper.message.vodto.MessageVO;
 
 public interface MessageDAO {
 	MessageVO selectMessageByNo(int messageNo) throws Exception;
+	List<MessageVO> selectMessagesByUserId(String userId, int startPoint, int amountToShow) throws Exception;
+	int insertIntoMessage(MessageDTO messageDTO) throws Exception;
 }
--- src/main/java/com/dodeveloper/message/dao/MessageDAOImpl.java
+++ src/main/java/com/dodeveloper/message/dao/MessageDAOImpl.java
@@ -1,14 +1,21 @@
 package com.dodeveloper.message.dao;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.dodeveloper.message.vodto.MessageDTO;
 import com.dodeveloper.message.vodto.MessageVO;
 
 @Repository
 public class MessageDAOImpl implements MessageDAO{
 
+	//insertIntoMessage
+	
 	@Autowired
 	private SqlSessionTemplate template;
 	
@@ -17,6 +24,22 @@ public class MessageDAOImpl implements MessageDAO{
 	@Override
 	public MessageVO selectMessageByNo(int messageNo) throws Exception {
 		return template.selectOne(namespace + ".selectMessageByNo", messageNo);
+	}
+
+	@Override
+	public List<MessageVO> selectMessagesByUserId(String userId, int startPoint, int amountToShow) throws Exception {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("userId", userId);
+		map.put("startPoint", startPoint);
+		map.put("amountToShow", amountToShow);
+		
+		return template.selectList(namespace + ".selectMessagesByUserId", map);
+	}
+
+	@Override
+	public int insertIntoMessage(MessageDTO messageDTO) throws Exception {
+		template.insert(namespace + ".insertIntoMessage", messageDTO);
+		return messageDTO.getMessageNo();
 	}
 
 }
