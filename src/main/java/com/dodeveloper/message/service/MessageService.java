89d96bb3a3ea86514ea807035153cf0509b098b7
kangjy <himgaso@gmail.com>
2024-05-03 14:31:38 +0900

    feat : message 송신 내역 확인 및 메세지 송신 로직 구현
    message 송신 내역을 확인하는 기능과 메세지를 송신하는 로직을
    service 단에 구현

--- src/main/java/com/dodeveloper/message/service/MessageService.java
+++ src/main/java/com/dodeveloper/message/service/MessageService.java
@@ -2,10 +2,14 @@ package com.dodeveloper.message.service;
 
 import java.util.List;
 
+import com.dodeveloper.message.vodto.MessageBoxDTO;
 import com.dodeveloper.message.vodto.MessageBoxVO;
+import com.dodeveloper.message.vodto.MessageDTO;
 import com.dodeveloper.message.vodto.MessageVO;
 
 public interface MessageService {
 	MessageVO getMessageByNo(int messageNo) throws Exception;
 	List<MessageVO> getReceivedMessages(String receiver, int startPoint, int amountToShow) throws Exception;
+	List<MessageVO> getSentMessages(String writer, int startPoint, int amountToShow) throws Exception;
+	void sendMessage(MessageDTO messageToSend, List<String> receiverIdList) throws Exception;
 }
--- src/main/java/com/dodeveloper/message/service/MessageServiceImpl.java
+++ src/main/java/com/dodeveloper/message/service/MessageServiceImpl.java
@@ -5,10 +5,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dodeveloper.message.dao.MessageBoxDAO;
 import com.dodeveloper.message.dao.MessageDAO;
+import com.dodeveloper.message.vodto.MessageBoxDTO;
 import com.dodeveloper.message.vodto.MessageBoxVO;
+import com.dodeveloper.message.vodto.MessageDTO;
 import com.dodeveloper.message.vodto.MessageVO;
 
 @Service
@@ -28,13 +33,29 @@ public class MessageServiceImpl implements MessageService{
 	@Override
 	public List<MessageVO> getReceivedMessages(String receiver, int startPoint, int amountToShow) throws Exception {
 		List<MessageVO> receivedMessages = new LinkedList<MessageVO>();
-		List<MessageBoxVO> receivedMessageInfo = messageBoxDAO.selectByReceiver(receiver, startPoint, amountToShow);
 		
-		for(MessageBoxVO messageInfo : receivedMessageInfo) {
+		for(MessageBoxVO messageInfo : messageBoxDAO.selectByReceiver(receiver, startPoint, amountToShow)) {
 			receivedMessages.add(messageDAO.selectMessageByNo(messageInfo.getMessageNo()));
 		}
 		
 		return receivedMessages;
+	}
+
+	@Override
+	public List<MessageVO> getSentMessages(String writer, int startPoint, int amountToShow) throws Exception {
+		return messageDAO.selectMessagesByUserId(writer, startPoint, amountToShow);
+	}
+
+	@Override
+	@Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.DEFAULT, rollbackFor = Exception.class)
+	public void sendMessage(MessageDTO messageToSend, List<String> receiverIdList) throws Exception {
+		
+		int pk = messageDAO.insertIntoMessage(messageToSend);
+		for(String receiver : receiverIdList) {
+			if(!messageBoxDAO.insert(new MessageBoxDTO(pk, receiver))) {
+				throw new Exception("insert into messageBox 에서 오류가 발생함");
+			}
+		}
 	}
 
 }
