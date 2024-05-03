50ccb95dfa20cf4c870d8f398018216ed7d0d40a
kjshsy0226 <kjshsy0226@gmail.com>
2024-05-03 14:48:53 +0900

    feat-mypage-kjs 마이페이지 관련 자바 파일 추가

---
+++ src/main/java/com/dodeveloper/mypage/dto/ProfileDTO.java
@@ -0,0 +1,19 @@
+package com.dodeveloper.mypage.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ProfileDTO {
+	private String userId;
+	private String prefix;
+	private byte[] profileImage;
+}
---
+++ src/main/java/com/dodeveloper/mypage/vo/ProfileVO.java
@@ -0,0 +1,18 @@
+package com.dodeveloper.mypage.vo;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ProfileVO {
+	private String prefix;
+	private byte[] profileImage;
+}
