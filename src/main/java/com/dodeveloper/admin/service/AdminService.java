5ab02ab210b1fc4a250505013f3612f96ebf956d
ljs <idid162@nate.com>
2024-05-03 18:08:24 +0900

    feat : 유저 조회 서비스단 작업중

--- src/main/java/com/dodeveloper/admin/service/AdminService.java
+++ src/main/java/com/dodeveloper/admin/service/AdminService.java
@@ -1,5 +1,10 @@
 package com.dodeveloper.admin.service;
 
-public class AdminService {
+import java.util.List;
 
+import com.dodeveloper.member.vo.MemberVO;
+
+public interface AdminService {
+	// 전체 유저를 조회하는 메서드
+	List<MemberVO> getAllUser() throws Exception;
 }
