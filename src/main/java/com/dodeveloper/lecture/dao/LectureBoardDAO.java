d39db4e8898765d383a865d77eeb9eb0e01bb1a8
kde <sue050533@naver.com>
2024-05-04 14:38:56 +0900

    feat : 게시판의 상세 게시글 조회를 위한 DAO단 완료

--- src/main/java/com/dodeveloper/lecture/dao/LectureBoardDAO.java
+++ src/main/java/com/dodeveloper/lecture/dao/LectureBoardDAO.java
@@ -8,4 +8,17 @@ public interface LectureBoardDAO {
 	
 	// 게시판의 글을 조회하는 메서드
 	List<LectureBoardVO> selectListAllLecBoard() throws Exception;
+
+	// ?번 글을 가져오는 메서드
+	LectureBoardVO selectBoardLecNo(int lecNo) throws Exception;
+	
+	// ?번 글을 ?유저가 조회했다는 이력을 기록하는 메서드
+	int insertReadCountProcess(String user, int lecNo) throws Exception;
+	
+	// 유저가 ?번 글을 언제 읽었는지 select하는 메서드
+	int selectDiff(String user, int lecNo) throws Exception;
+	
+	// ?번 글의 조회수를 증가하는 메서드
+	int updateReadCount(int lecNo) throws Exception;
+	
 }
--- src/main/java/com/dodeveloper/lecture/dao/LectureBoardDAOImpl.java
+++ src/main/java/com/dodeveloper/lecture/dao/LectureBoardDAOImpl.java
@@ -1,6 +1,8 @@
 package com.dodeveloper.lecture.dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,12 +24,78 @@ public class LectureBoardDAOImpl implements LectureBoardDAO {
 	 * @date : 2024.05.02
 	 * @param : 
 	 * @return : List<LectureBoardVO>
-	 * @description : DAO
+	 * @description : 게시판 전체 조회에 대한 DAO 메서드
 	 */
 	@Override
 	public List<LectureBoardVO> selectListAllLecBoard() throws Exception {
 		
 		return ses.selectList(ns + ".getAllBoard");
+	}
+
+	/**
+	 * @methodName : selectBoardByBoardNo
+	 * @author : kde
+	 * @date : 2024.05.03
+	 * @param : int lecNo - 게시글 번호
+	 * @return : 
+	 * @description : ?번 글을 가져오는 메서드
+	 */
+	@Override
+	public LectureBoardVO selectBoardLecNo(int lecNo) throws Exception {
+		
+		return ses.selectOne(ns + ".selectBoardLecNo", lecNo);
+	}
+
+	/**
+	 * @methodName : insertReadCountProcess
+	 * @author : kde
+	 * @date : 2024.05.03
+	 * @param : String user - 글을 조회한 유저
+	 * @param : int lecNo - 게시글
+	 * @return : int
+	 * @description : ?번 글을 ?유저가 조회했다는 이력을 기록하는 메서드
+	 */
+	@Override
+	public int insertReadCountProcess(String user, int lecNo) throws Exception {
+		Map<String, Object> params = new HashMap<String, Object>();
+		
+		params.put("user", user);
+		params.put("lecNo", lecNo);
+		
+		return ses.insert(ns + ".insertReadCountProcess", params);
+	}
+
+	/**
+	 * @methodName : selectDiff
+	 * @author : kde
+	 * @date : 2024.05.03
+	 * @param : String user - 글을 조회한 유저
+	 * @param : int lecNo - 게시글
+	 * @return : int
+	 * @description : 유저가 ?번 글을 언제 읽었는지 select하는 메서드
+	 */
+	@Override
+	public int selectDiff(String user, int lecNo) throws Exception {
+		Map<String, Object> params = new HashMap<String, Object>();
+		
+		params.put("user", user);
+		params.put("lecNo", lecNo);
+		
+		return ses.selectOne(ns + ".getDateDiff", params);
+	}
+
+	/**
+	 * @methodName : updateReadCount
+	 * @author : kde
+	 * @date : 2024.05.03
+	 * @param : int lecNo - 게시글
+	 * @return : int
+	 * @description : ?번 글의 조회수를 증가하는 메서드
+	 */
+	@Override
+	public int updateReadCount(int lecNo) throws Exception {
+		
+		return ses.update(ns + ".updateReadCount", lecNo);
 	}
 
 }
