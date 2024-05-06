991517249dfb1f806676dc6b83257c8c2b8ecc09
kimso05 <web.ksyoung0513@gmail.com>
2024-05-06 22:03:28 +0900

    feat : 기업 리뷰 보기 생성함

--- src/main/java/com/dodeveloper/company/service/CompanyInfoService.java
+++ src/main/java/com/dodeveloper/company/service/CompanyInfoService.java
@@ -3,8 +3,12 @@ package com.dodeveloper.company.service;
 import java.util.List;
 
 import com.dodeveloper.company.vodto.CompanyInfoVO;
+import com.dodeveloper.company.vodto.revCompanyBoardVO;
 
 public interface CompanyInfoService {
 	// 기업 정보 전체 조회하는 메서드
 	List<CompanyInfoVO> getEntireCompanyInfo() throws Exception;
+	
+	// 클릭한 기업 리뷰 조회하는 메서드
+	List<revCompanyBoardVO> getCompanyInfoRev(int companyInfoNo) throws Exception;
 }
