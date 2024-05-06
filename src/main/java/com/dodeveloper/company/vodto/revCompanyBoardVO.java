d31c30227b870c5fe01809eb2d77178ac481c550
kimso05 <web.ksyoung0513@gmail.com>
2024-05-06 23:36:18 +0900

    feat : 기업 리뷰 페이지 필요한 정보들 revCompanyBoardVO단에 생성함

---
+++ src/main/java/com/dodeveloper/company/vodto/revCompanyBoardVO.java
@@ -0,0 +1,25 @@
+package com.dodeveloper.company.vodto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class revCompanyBoardVO {
+	private int revNo;
+	private String revWriter;
+	private String revTitle;
+	private String revProfession;
+	private String revContent;
+	private String revGood;
+	private String revBed;
+	private int companyInfoNo;
+	private int bType;
+}
