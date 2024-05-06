9d1484bab44bb56d690c17ba91dc427cc9dcad44
kde <sue050533@naver.com>
2024-05-06 18:56:21 +0900

    feat : 검색 조건 수정 + 검색 필터(최신순 / 인기순 / 조회순) controller단 완료

--- src/main/java/com/dodeveloper/lecture/controller/LectureBoardController.java
+++ src/main/java/com/dodeveloper/lecture/controller/LectureBoardController.java
@@ -1,6 +1,7 @@
 package com.dodeveloper.lecture.controller;
 
 import java.util.List;
+
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,7 +11,6 @@ import javax.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -35,40 +35,57 @@ public class LectureBoardController {
 	@Autowired
 	private LectureBoardService lService; // 스프링 컨테이너에서 LectureService 객체를 찾아 주입
 
+	// 생성자를 이용하여 객체 주입
+	public LectureBoardController(LectureBoardService service) {
+		this.lService = service;
+	}
+	
 	/**
 	 * @methodName : listAllGet
 	 * @author : kde
 	 * @date : 2024.05.02
 	 * @param : Model model : View단(listAll)으로 바인딩하는 전용 객체
 	 * @param : @RequestParam(value = "lecNo", defaultValue = "1") int lecNo
-	 * int 매개변수 'lecNo'가 존재하지만 기본 유형으로 선언되기 때문에 null값으로 변환할 수 없어서 객체 래퍼로 선언한 것
+	 * 			int 매개변수 'lecNo'가 존재하지만 기본 유형으로 선언되기 때문에 null값으로 변환할 수 없어서 객체 래퍼로 선언한 것
 	 * @param : LectureSearchDTO lsDTO - 검색어 Type와 Value를 가져옴
 	 * @return : void
 	 * @throws Exception
-	 * @description : 강의 추천 게시판 전체 글 조회를 담당하는 controller 메서드
+	 * @description : 1) 강의 추천 게시판 전체 글 조회를 담당하는 controller 메서드
+	 * 2) 검색 조건 : 검색을 했을 경우 - 제목 / 작성자 / 내용
 	 */
 	@GetMapping(value = "/listAll")
-	public void listAllBoardGet(Model model, @RequestParam(value = "searchType", required = false) String searchType,
-            @RequestParam(value = "searchValue", required = false) String searchValue, LectureSearchDTO lsDTO) throws Exception {
-		logger.info("강의 추천 게시판 전체 게시글 조회 : listAll View");
-		logger.info("검색어 : " + lsDTO.toString());
-		
-		List<LectureBoardVO> lectureBoardList = null;
-		
-		if (searchType != null && searchValue != null) {
-			// 검색 조건이 있을 경우
-			lsDTO.setSearchType(searchType);
-			lsDTO.setSearchValue(searchValue);
-			lectureBoardList = lService.getListAllBoard(1, lsDTO);
-		} else {
-			// 검색 조건이 없을 경우
-			lectureBoardList = lService.getListAllBoard(1, lsDTO);
-		}
-		
-		// 바인딩
-		model.addAttribute("lectureBoardList", lectureBoardList);
-		
+	public void listBoardGet(Model model,
+	        @RequestParam(value = "searchType", required = false) String searchType,
+	        @RequestParam(value = "searchValue", required = false) String searchValue,
+	        @RequestParam(value = "filterType", required = false) String filterType,
+	        LectureSearchDTO lsDTO) throws Exception {
+	    
+	    List<LectureBoardVO> lectureBoardList = null;
+	    
+	    // 검색 필터와 검색 조건을 동시에 처리
+	    if (searchType != null && searchValue != null) {
+	    	logger.info("검색 조건을 선택하고 검색어를 입력했어요!");
+	        // 검색 조건이 있는 경우
+	        lsDTO.setSearchType(searchType);
+	        lsDTO.setSearchValue(searchValue);
+	        lectureBoardList = lService.listAllBoardBySearch(1, lsDTO);
+	    } else {
+	        // 검색 조건이 없는 경우
+	        lectureBoardList = lService.getListAllBoard(1);
+	    }
+	    
+	    // 검색 필터 적용
+	    if (filterType != null && !filterType.isEmpty()) {
+	    	logger.info("검색 필터를 선택했어요!");
+	        // 필터가 존재하면 해당 필터에 따라 게시글을 가져옴
+	        lectureBoardList = lService.listAllBoardByFilter(lectureBoardList, filterType);
+	    }
+	    
+	    // 바인딩
+	    model.addAttribute("lectureBoardList", lectureBoardList);
+
 	}
+
 
 	/**
 	 * @methodName : viewBoard
@@ -77,7 +94,7 @@ public class LectureBoardController {
 	 * @param : @RequestParam("lecNo") int lecNo : 조회하려는 글 번호
 	 * @return : ModelAndView : 조회하는 글을 바인딩 한 후 게시판 상세 페이지로 이동시키는 객체
 	 * @throws Exception
-	 * @description :
+	 * @description : 유저가 lecNo번 글을 눌렀을 때 상세 페이지로 가는 메서드
 	 */
 	@GetMapping("/viewBoard") // 상세 게시글로 가도록 Mapping함
 	public ModelAndView viewBoard(@RequestParam("lecNo") int lecNo, HttpServletRequest req, HttpServletResponse resp,
@@ -199,10 +216,10 @@ public class LectureBoardController {
 
 		return "redirect:/lecture/listAll";
 	}
-	
+
 	/**
 	 * @methodName : cancelBoard
-	 * @author : 
+	 * @author :
 	 * @date : 2024.05.05
 	 * @return : String
 	 * @description : 유저가 게시글을 작성하려다 취소버튼을 누른 경우에 작동되는 메서드
@@ -210,7 +227,7 @@ public class LectureBoardController {
 	@RequestMapping(value = "/cancel", method = RequestMethod.POST, produces = "text/plain; charset=utf-8")
 	public @ResponseBody String cancelBoard() {
 		System.out.println("게시글 작성 안할래요!");
-		
+
 		return "success";
 	}
 
