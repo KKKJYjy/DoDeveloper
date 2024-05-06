288d5620245f67866c1007423c42d76c554d30d7
kde <sue050533@naver.com>
2024-05-06 18:54:58 +0900

    feat : 검색 조건 메서드 외 수정 + 검색 필터(최신순 / 인기순 / 조회순) service단 완료

--- src/main/java/com/dodeveloper/lecture/service/LectureBoardService.java
+++ src/main/java/com/dodeveloper/lecture/service/LectureBoardService.java
@@ -1,6 +1,7 @@
 package com.dodeveloper.lecture.service;
 
 import java.util.List;
+
 import java.util.Map;
 
 import com.dodeveloper.lecture.vodto.LectureBoardDTO;
@@ -10,8 +11,8 @@ import com.dodeveloper.lecture.vodto.LectureSearchDTO;
 public interface LectureBoardService {
 
 	// 게시판의 lecNo번 페이지의 글을 조회하는 메서드
-	List<LectureBoardVO> getListAllBoard(int lecNo, LectureSearchDTO lsDTO) throws Exception;
-
+	List<LectureBoardVO> getListAllBoard(int lecNo) throws Exception;
+	
 	// 게시글을 상세 조회하는 메서드(조회수를 올려야할지 말아야할지 검사하여 조회수 업 하는 기능까지)
 	Map<String, Object> getBoardByBoardNo(int lecNo, String user) throws Exception;
 
@@ -26,5 +27,11 @@ public interface LectureBoardService {
 
 	// 게시글을 삭제 처리하는 메서드
 	void deleteLectureBoard(int lecNo) throws Exception;
+	
+	// 검색 조건을 선택하고 검색어를 입력했을 때 글을 가져오는 메서드 - 검색 조건
+	List<LectureBoardVO> listAllBoardBySearch(int lecNo, LectureSearchDTO lsDTO) throws Exception;
 
+	// 검색 필터(최신순 / 인기순 / 조회순)을 선택했을 때 글을 가져오는 메서드 - 검색 필터
+	List<LectureBoardVO> listAllBoardByFilter(List<LectureBoardVO> lectureBoardList, String filterType) throws Exception;
+	
 }
--- src/main/java/com/dodeveloper/lecture/service/LectureBoardServiceImpl.java
+++ src/main/java/com/dodeveloper/lecture/service/LectureBoardServiceImpl.java
@@ -22,23 +22,59 @@ public class LectureBoardServiceImpl implements LectureBoardService {
 	 * @methodName : getListAllBoard
 	 * @author : kde
 	 * @date : 2024.05.02
-	 * @param : 
+	 * @param : int lecNo - 게시글 번호
 	 * @return : List<LectureBoardVO>
 	 * @description : 게시판 전체 조회에 대한 서비스 메서드
 	 */
 	@Override
-	public List<LectureBoardVO> getListAllBoard(int lecNo, LectureSearchDTO lsDTO) throws Exception {
-	    System.out.println("서비스단 : 페이지 전체 게시글 조회!");
-	    
-	    List<LectureBoardVO> lecBoardList = null;
-	    
-	    if (lsDTO.getSearchType() != null && lsDTO.getSearchValue() != null) {
-	        lecBoardList = lDao.lectureBoardListWithSc(lsDTO);
-	    } else {
-	        lecBoardList = lDao.selectListAllLecBoard();
-	    }
-	    
-	    return lecBoardList;
+	public List<LectureBoardVO> getListAllBoard(int lecNo) throws Exception {
+		System.out.println("서비스단 : 페이지 전체 게시글 조회!");
+
+		// DAO단 호출 (selectListAllLecBoard() 메서드 호출)
+		List<LectureBoardVO> lecBoardList = null;
+
+		return lDao.selectListAllLecBoard();
+
+	}
+
+	/**
+	 * @methodName : listAllBoardBySearch
+	 * @author : kde
+	 * @date : 2024.05.06
+	 * @param : int lecNo - 게시글 번호
+	 * @param : LectureSearchDTO lsDTO - 검색할 때 가져올 Type, Value
+	 * @return : List<LectureBoardVO>
+	 * @description : 검색 조건을 선택하고 검색어를 입력했을 때 글을 가져오는 메서드 - 검색 조건
+	 */
+	@Override
+	public List<LectureBoardVO> listAllBoardBySearch(int lecNo, LectureSearchDTO lsDTO) throws Exception {
+		System.out.println("검색조건을 선택하고 검색어를 입력했습니다!");
+
+		// 검색 조건
+		// 검색어가 있는지 없는지 판단
+		if (lsDTO.getSearchType() != null && lsDTO.getSearchValue() != null) {
+			// 검색어가 있는 경우
+			return lDao.lectureBoardListWithSc(lsDTO);
+		} else {
+			// 검색어가 없는 경우
+			return lDao.selectListAllLecBoard();
+			
+		}
+	}
+	
+	/**
+	 * @methodName : listAllBoardByFilter
+	 * @author : kde
+	 * @date : 2024.05.06
+	 * @param : List<LectureBoardVO> lectureBoardList - 게시글 목록
+	 * @param : String filterType - 필터 타입(최신순 / 인기순 / 조회순)
+	 * @return : List<LectureBoardVO>
+	 * @description : 검색 필터(최신순 / 인기순 / 조회순)을 선택했을 때 글을 가져오는 메서드 - 검색 필터
+	 */
+	@Override
+	public List<LectureBoardVO> listAllBoardByFilter(List<LectureBoardVO> lectureBoardList, String filterType) throws Exception {
+		
+		return lDao.listAllBoardByFilter(lectureBoardList, filterType);
 	}
 
 	/**
@@ -48,27 +84,26 @@ public class LectureBoardServiceImpl implements LectureBoardService {
 	 * @param : int boardNo - 조회할 글 번호
 	 * @param : String user - 조회하는 유저 (로그인 하지 않았을 경우 session id)
 	 * @return : Map<String, Object>
-	 * @description : 
-	 * 1) user가 조회하는 글을 하루 이내에 읽은 적이 있는지 없는지 검사
+	 * @description : 1) user가 조회하는 글을 하루 이내에 읽은 적이 있는지 없는지 검사
 	 * 2) 하루 이내에 읽은 적이 없다 -> 조회수 증가(update), 조회이력 - 기록을 남긴다(insert), 글 가져옴(유저가 글을 조회하도록)
 	 * 3) 하루 이내에 읽은 적이 있다 -> 게시글만 가져옴(유저가 글을 조회하도록)
 	 */
 	@Override
 	public Map<String, Object> getBoardByBoardNo(int lecNo, String user) throws Exception {
 		System.out.println(lDao.selectDiff(user, lecNo));
-		
+
 		// 조회된 글 가져오기
 		LectureBoardVO lecBoard = lDao.selectBoardLecNo(lecNo);
-		
+
 		Map<String, Object> result = new HashMap<String, Object>();
-		
+
 		result.put("lecBoard", lecBoard); // 조회된 글 바인딩
-		
+
 //		
 //		if (lDao.selectDiff(user, lecNo) == -1) {
 //			// 하루 이내에 읽은 적이 없을 경우
 //		}
-		
+
 		return result;
 	}
 
@@ -82,16 +117,16 @@ public class LectureBoardServiceImpl implements LectureBoardService {
 	 */
 	@Override
 	public Map<String, Object> getBoardByBoardNo(int lecNo) throws Exception {
-		
+
 		// DAO단에서 조회된 글 가져오기
 		LectureBoardVO lecBoard = lDao.selectBoardLecNo(lecNo);
-		
+
 		System.out.println("조회된 글 : " + lecBoard.toString());
-		
+
 		Map<String, Object> result = new HashMap<String, Object>();
-		
+
 		result.put("lecBoard", lecBoard);
-		
+
 		return result;
 	}
 
@@ -105,14 +140,14 @@ public class LectureBoardServiceImpl implements LectureBoardService {
 	 */
 	@Override
 	public boolean writeBoardService(LectureBoardDTO newLecBoard) throws Exception {
-		
+
 		boolean result = false;
-		
+
 		// dao단 호출
 		if (lDao.insertNewLectureBoard(newLecBoard) == 1) {
 			result = true;
 		}
-		
+
 		return result;
 	}
 
@@ -126,9 +161,9 @@ public class LectureBoardServiceImpl implements LectureBoardService {
 	 */
 	@Override
 	public void modifyBoard(LectureBoardDTO modifyBoard) throws Exception {
-		
+
 		lDao.updateLectureBoard(modifyBoard);
-		
+
 	}
 
 	/**
@@ -141,9 +176,9 @@ public class LectureBoardServiceImpl implements LectureBoardService {
 	 */
 	@Override
 	public void deleteLectureBoard(int lecNo) throws Exception {
-		
+
 		lDao.deleteLectureBoard(lecNo);
-		
+
 	}
 
 }
