2a462c2fc15e64cbfcac0ef80d5cd957b7c9280c
kangjy <himgaso@gmail.com>
2024-05-08 15:34:09 +0900

    feat : FileProcessing 을 bean 등록, 여러 업로드 기능 추가
    static 하게 만든 유틸 함수 호출용 클래스 FileProcessing 를 static 을 없애고 스프링 bean으로 전환
    realPath를 인스턴스 생성 시기에 받아오는 것으로 전환.
    tempUpload 및 uploadFiles 폴더에 직접 파일을 생성하는 기능 추가

--- src/main/java/com/dodeveloper/etc/FileProcessing.java
+++ src/main/java/com/dodeveloper/etc/FileProcessing.java
@@ -3,11 +3,41 @@ package com.dodeveloper.etc;
 import java.io.File;
 import java.util.UUID;
 
+import javax.servlet.ServletContext;
+
 import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+@Component
 public class FileProcessing {
-	public static String moveFile(String fileName, String from, String dest) throws Exception {
+
+	private String realPath;
+
+	@Autowired
+	public FileProcessing(ServletContext servletContext) {
+		realPath = servletContext.getRealPath("");
+		File realUploadPath = new File(realPath + File.separator + UploadPaths.realUploadPath);
+		File tempUploadPath = new File(realPath + File.separator + UploadPaths.tempUploadPath);
+		realUploadPath.mkdir();
+		tempUploadPath.mkdir();
+	}
+
+	public String getRealPath() {
+		return realPath;
+	}
+	
+	public String saveTempFilePermanantly(String fileName) throws Exception {
+		return moveFile(fileName, realPath + File.separator + UploadPaths.tempUploadPath,
+				realPath + File.separator + UploadPaths.realUploadPath);
+	}
+
+	public String uploadFileTemporarily(MultipartFile file) throws Exception {
+		return fileUpload(file, realPath + File.separator + UploadPaths.tempUploadPath);
+	}
+
+	public String moveFile(String fileName, String from, String dest) throws Exception {
 
 		File oldFile = new File(from + File.separator + fileName);
 		if (!oldFile.exists()) {
@@ -18,29 +48,28 @@ public class FileProcessing {
 		File newFile = new File(dest + File.separator + movedFileName);
 
 		FileUtils.moveFile(oldFile, newFile);
-		
+
 		return movedFileName;
 	}
 
-	public static String fileUpload(MultipartFile file, String uploadPath)
-			throws Exception {
-		
+	public String fileUpload(MultipartFile file, String uploadPath) throws Exception {
+
 		String originalName = file.getOriginalFilename();
 		String uploadName = createUniqueName(modifyFileName(originalName), uploadPath);
 
 		File saveFile = new File(uploadPath + File.separator + uploadName);
 		file.transferTo(saveFile);
-		
+
 		return uploadName;
 	}
 
-	private static String modifyFileName(String fileName) {
+	private String modifyFileName(String fileName) {
 		String uuid = UUID.randomUUID().toString().replace("-", "");
 
 		return uuid + "_" + fileName;
 	}
 
-	private static String createUniqueName(String fileName, String filePath) throws Exception {
+	private String createUniqueName(String fileName, String filePath) throws Exception {
 		if ((new File(filePath + File.separator + fileName).exists()) == false) {
 			return fileName;
 		}
@@ -48,21 +77,25 @@ public class FileProcessing {
 		String ext = getExtension(fileName);
 		String nameWithoutExt = getNameWithoutExt(fileName);
 
-		for (int i = 0; i < 9; i++) {
+		for (int i = 0; i <= 999; i++) {
 			String newName = nameWithoutExt + "_" + i + "." + ext;
 			if ((new File(filePath + File.separator + newName)).exists() == false) {
 				return newName;
 			}
 		}
-		
+
 		throw new Exception("Failed to create unique File name");
 	}
 
-	public static String getExtension(String fileName) {
+	public String getExtension(String fileName) {
 		return fileName.substring(fileName.lastIndexOf(".") + 1);
 	}
 
-	public static String getNameWithoutExt(String fileName) {
+	public String getNameWithoutExt(String fileName) {
 		return fileName.substring(0, fileName.lastIndexOf("."));
+	}
+	
+	public String getOriginalName(String modifiedName) {
+		return modifiedName.substring(modifiedName.indexOf("_") + 1);
 	}
 }
