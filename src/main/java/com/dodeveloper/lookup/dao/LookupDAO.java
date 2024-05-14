092dfe24ecdf2afa4c7b2ecfc030186334197a34
webyyj <duswn7081@naver.com>
2024-05-14 20:38:33 +0900

    feat : 조회수 증가되는 공통 로직 구현

---
+++ src/main/java/com/dodeveloper/lookup/dao/LookupDAO.java
@@ -0,0 +1,12 @@
+package com.dodeveloper.lookup.dao;
+
+public interface LookupDAO {
+	//해당 유저가 ?게시판 ?번글을 언제 읽었는지 확인하는 메서드 (공통)
+	int selectDiff(String readWho, int boardNo, int bType) throws Exception;
+		
+	//해당 유저가 ?게시판 ?번 게시글을 조회했다는 이력을 insert 하는 메서드 (공통)
+	int insertLookup(String readWho, int boardNo, int bType) throws Exception;
+	
+	//스터디 게시판의 ?번글의 조회수를 올리는 메서드 (각자구현)
+	int updateLookupStudyBoard(int stuNo) throws Exception;
+}
