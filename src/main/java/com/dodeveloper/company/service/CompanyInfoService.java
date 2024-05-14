b8a5ca834918012fa787289c4d100d01d4816cdb
kimso05 <web.ksyoung0513@gmail.com>
2024-05-15 00:36:21 +0900

    feat : 기업 리뷰 게시글 저장 작업함

--- src/main/java/com/dodeveloper/company/service/CompanyInfoService.java
+++ src/main/java/com/dodeveloper/company/service/CompanyInfoService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.dodeveloper.company.vodto.CompanyInfoVO;
 import com.dodeveloper.company.vodto.RevCompanyBoardVO;
+import com.dodeveloper.company.vodto.WrittenCompanyBoardDTO;
 
 public interface CompanyInfoService {
 	// 기업 정보 전체 조회하는 메서드
@@ -11,4 +12,7 @@ public interface CompanyInfoService {
 	
 	// 클릭한 기업 리뷰 조회하는 메서드
 	List<RevCompanyBoardVO> getCompanyInfoRev(int companyInfoNo) throws Exception;
+	
+	// 기업 리뷰 게시글을 저장하는 메서드
+	int writeCompanyBoardService(WrittenCompanyBoardDTO newWrittenCompanyBoard) throws Exception;
 }
