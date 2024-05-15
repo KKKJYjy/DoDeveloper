9bf3d2782b30fd7c1aa5c2b9c33083862ffc6080
kde <sue050533@naver.com>
2024-05-15 17:00:32 +0900

    feat : 검색 필터(최신순 / 인기순 / 조회순) 따로 사용하다가 페이징때문에 합침

--- src/main/java/com/dodeveloper/lecture/vodto/LectureSearchDTO.java
+++ src/main/java/com/dodeveloper/lecture/vodto/LectureSearchDTO.java
@@ -15,4 +15,5 @@ import lombok.ToString;
 public class LectureSearchDTO {
 	private String searchType; // 검색 조건
 	private String searchValue; // 검색 값
+	private String filterType; // 검색 필터(최신순 / 인기순 / 조회순)
 }
