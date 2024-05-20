88746add5acabf7407941e17c3cdbf46ee8f50ca
webyyj <duswn7081@naver.com>
2024-05-20 22:36:35 +0900

    feat: 스터디 언어 수정할때 사용할 플래그 변수 추가

--- src/main/java/com/dodeveloper/study/vodto/StuStackModifyDTO.java
+++ src/main/java/com/dodeveloper/study/vodto/StuStackModifyDTO.java
@@ -19,6 +19,6 @@ public class StuStackModifyDTO {
 	private int[] chooseStack;
 
 	// 게시글 수정시 파일 업로드 수정할때 필요한 플래그 멤버 변수
-	// private boolean isDelete; // 게시글 수정시 유저가 삭제 처리하겠다 표시한 언어 (true: 수정 누르면 DB에서 삭제 false: 아무것도 안함)
-	// private boolean isNew; // 게시글 수정시 새롭게 추가된 언어 (true: 수정버튼 누르면 새롭게 insert, false: 아무것도안함)
+	private boolean isDelete; // 게시글 수정시 유저가 삭제 처리하겠다 표시한 언어 (true: 수정 누르면 DB에서 삭제 false: 아무것도 안함)
+	private boolean isNew; // 게시글 수정시 새롭게 추가된 언어 (true: 수정버튼 누르면 새롭게 insert, false: 아무것도안함)
 }
