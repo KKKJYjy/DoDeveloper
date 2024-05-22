fdf815878c083af66839d9466fdd2a96628cf48f
kangjy <himgaso@gmail.com>
2024-05-22 13:12:50 +0900

    fix: 파일 이름 변경 관련 버그 수정
    파일의 확장자가 없을 때 생길 수 있는 예외를 처리함
    추가로 파일의 경로 문자으을 운영체제로부터 독립적이게끔 수정함

--- src/main/java/com/dodeveloper/etc/UploadPaths.java
+++ src/main/java/com/dodeveloper/etc/UploadPaths.java
@@ -1,6 +1,8 @@
 package com.dodeveloper.etc;
 
+import java.io.File;
+
 public interface UploadPaths {
-	public static final String tempUploadPath = "resources\\tempUpload";
-	public static final String realUploadPath = "resources\\uploadFiles";
+	public static final String tempUploadPath = "resources" + File.separator + File.separator + "tempUpload";
+	public static final String permanentUploadPath = "resources" + File.separator + File.separator + "uploadFiles";
 }
